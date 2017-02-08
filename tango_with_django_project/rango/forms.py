7666757b69e38c0a186da1e37c12c1619acccda2
Shuangyue Li <2258561l@student.gla.ac.uk>
2017-02-08 23:01:10 +0000

    9.6 register done

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,7 +1,9 @@
 #store form related classes
 
 from django import forms
+from django.contrib.auth.models import User
 from rango.models import Page, Category
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text = "Please enter the category name.")
@@ -15,7 +17,6 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 
-
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text = "Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
@@ -24,7 +25,6 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
-
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -35,3 +35,17 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
             return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,6 +4,8 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -72,6 +74,48 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form,'category':category}
     return render(request, 'rango/add_page.html', context_dict)
 
+def register(request):
+    #a boolean value for telling the template if the registration was successful.
+    #set to false initially, change to true when registration succeeds.
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+
+        if user_form.is_vaild() and profile_form.is_vaild():
+
+            user = user_form.save()
+
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+                  'rango/register.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered':registered})
 
 
 
