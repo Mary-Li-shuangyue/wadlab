f5ffcef593ee834259343c9f45c786c5ba95f6d9
Shuangyue Li <2258561l@student.gla.ac.uk>
2017-02-09 03:07:23 +0000

    10 kinda done??

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -32,7 +32,11 @@ def about(request):
     if request.session.test_cookie_worked():
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
-    return render(request, 'rango/about.html')
+
+    visitor_cookie_handler(request)
+    context_dict = {'visits': request.session['visits']}
+
+    return render(request, 'rango/about.html' , context = context_dict)
 
 
 def show_category(request, category_name_slug):
